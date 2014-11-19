8379a28eaea9fc1bda4ebaa2a0d526ac7a905a1d
Haochen Xie <haochen@xie.name>
2014-11-19 15:52:30 +0900

    Add WorldCanvas's skeleton
    Signed-off-by: Haochen Xie <haochen@xie.name>

---
+++ src/main/java/name/haochenxie/tryout/mark_interview_question2/task1/WorldCanvas.java
@@ -0,0 +1,56 @@
+package name.haochenxie.tryout.mark_interview_question2.task1;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Point;
+import java.awt.Rectangle;
+
+import javax.swing.JPanel;
+
+public class WorldCanvas extends JPanel {
+	
+	private static final long serialVersionUID = -955003109057302468L;
+	
+	protected Point offset;
+	
+	public WorldCanvas(int offset_x, int offset_y) {
+		this.offset = new Point(offset_x, offset_y);
+	}
+	
+	public WorldCanvas() {
+		this(0, 0);
+	}
+	
+	public Point getOffset() {
+		return offset;
+	}
+	
+	public void setOffset(Point offset) {
+		this.offset = offset;
+		repaint();
+	}
+	
+	@Override
+	protected void paintComponent(Graphics g) {
+		
+	}
+	
+	protected Rectangle getView() {
+		// TODO not implemented
+		return null;
+	}
+	
+	protected void drawGrid(Graphics g) {
+		// TODO not implemented
+	}
+	
+	protected void drawCircle(Point pos, double radius, Color color) {
+		// TODO not implemented
+	}
+
+	protected boolean isCircleVisible(Point pos, double radius) {
+		// TODO not implemented
+		return false;
+	}
+	
+}
